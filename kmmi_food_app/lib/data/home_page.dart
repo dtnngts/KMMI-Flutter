bb1cbc7da8a105ec570023bcdccbdaf60f71bdb5
dtnngts <dota.ningtias100219@students.unila.ac.id>
2021-09-13 22:12:28 +0700

    first commit

---
+++ kmmi_food_app/lib/data/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatefulWidget {
+  const ({ Key? key }) : super(key: key);
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
